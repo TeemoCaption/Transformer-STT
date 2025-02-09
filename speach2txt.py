846f8a2665f879a78f24e8914274dffcc36a184c
TeemoCaption <eyan0816@gmail.com>
2025-02-09 18:48:44 +0800

    update speach2txt.py

--- speach2txt.py
+++ speach2txt.py
@@ -4,12 +4,65 @@ import random
 from glob import glob
 import model as md
 import preprocess as pre
+import tensorflow as tf
 
+# 創建文本數據集
+def create_text_ds(data):
+    texts = ["text" for _ in data]
+    text_ds = [vectorizer(t) for t in texts]
+    # tf.data.Dataset.from_tensor_slices 創建數據集
+    text_ds = tf.data.Dataset.from_tensor_slices(text_ds)
+    return text_ds
+
+def path_to_audio(path):
+    # 使用 stft 的頻譜圖
+    # tf.io.read_file 讀取音頻文件
+    audio = tf.io.read_file(path)
+    # tf.audio.decode_wav 解碼音頻文件，1 表示只讀取單聲道
+    audio, _ = tf.audio.decode_wav(audio, 1)
+    # tf.squeeze 用來移除指定維度大小為 1 的維度，axis=-1 代表對最後一個維度進行操作
+    audio = tf.squeeze(audio, axis=-1)
+    # tf.signal.stft 使用短時傅立葉變換將音頻轉換為頻譜圖
+    # frame_length 表示每一幀的長度，frame_step 表示每一幀之間的間隔，fft_length 表示傅立葉變換的點數
+    stfts = tf.signal.stft(audio, frame_length=200, frame_step=80, fft_length=256)
+    # tf.math.pow 對 stfts 的絕對值進行 0.5 次方運算
+    x = tf.math.pow(tf.abs(stfts), 0.5)
+
+    # tf.math.reduce_mean 求平均值，axis=1 表示對第二個維度進行操作，keepdims=True 表示保持維度
+    means = tf.math.reduce_mean(x, 1, keepdims=True)
+    # tf.math.reduce_std 求標準差
+    stddevs = tf.math.reduce_std(x, 1, keepdims=True)
+    # 標準化
+    x = (x - means) / stddevs
+
+    audio_len = tf.shape(x)[0]
+    # 填充至 10 秒
+    pad_len = 2754
+    # tf.constant 是用來創建一個常數張量的函數
+    # 第一行 [0, pad_len] 表示對第一個維度（時間或長度維度）進行填充，填充的方式是 在起始位置不進行填充（0），在結尾位置填充 pad_len 長度的空間
+    # 第二行 [0, 0] 表示對第二個維度（頻率維度）不進行填充
+    paddings = tf.constant([[0, pad_len], [0, 0]])
+    # tf.pad 用來將張量 x 進行填充操作
+    # "CONSTANT" 表示填充的方式是用常數填充
+    x = tf.pad(x, paddings, "CONSTANT")[:pad_len, :]
+
+    return x
+
+# 創建音頻數據集
+def create_audio_ds(data):
+    flist = ["audio" for _ in data]
+    # tf.data.Dataset.from_tensor_slices 創建數據集
+    audio_ds = tf.data.Dataset.from_tensor_slices(flist)
+    audio_ds = audio_ds.map(
+        path_to_audio, num_parallel_calls=tf.data.AUTOTUNE # 使用多線程加速
+    )
+    return audio_ds
 
 def main():
     path = "./dataset/"
     # 獲取所有的wav文件，recursive 表示是否進行遞迴搜尋
-    wavs = glob(path + "wavs/*.wav", recursive=True)
+    wavs = [w.replace("\\", "/") for w in glob(path + "wavs/*.wav", recursive=True)]
+    
     id2text = {}
     with open(os.path.join(path, "metadata.csv"), encoding="utf-8") as f:
         for line in f:
@@ -25,8 +78,8 @@ def main():
     data = pre.get_data(wavs, id2text, maxlen=max_target_len)
     vectorizer = pre.VectorizeChar(max_target_len)
     print("Vocab size", len(vectorizer.get_vocabulary()))
-    print("WAV files:", wavs)
-    print("\n", data[0])
+    #print("\n", vectorizer.get_vocabulary())
+    print("\n", len(vectorizer('t')))
     
 if __name__ == "__main__":
     main()    
